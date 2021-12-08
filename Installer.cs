8388dc86e3003f2e8c44fd4f91d23c7859a628ce
Lythium <lythiumyt@gmail.com>
2021-12-08 17:18:44 +0000

    delete current file if it already exists

--- Installer.cs
+++ Installer.cs
@@ -9,6 +9,11 @@ public static class Installer
     public static void Download(string url, string packageName, string gmodPath)
     {
         WebClient client = new WebClient();
+        if (File.Exists(packageName))
+        {
+            File.Delete(packageName+"_win32.dll");
+            File.Delete(packageName+"_win64.dll");
+        }
         client.DownloadFile("https://github.com/"+url+packageName+"_win32", gmodPath+packageName+"_win32.dll");
         client.DownloadFile("https://github.com/"+url+packageName+"_win64", gmodPath+packageName+"_win64.dll");
         Console.WriteLine("Module Successfully Installed!".Pastel(Color.Green));
